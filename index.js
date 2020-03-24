12ba936d7c830604ee3c8efab3276fed3feb1c82
Azdeman <cyber_azdeman@mail.ru>
2020-03-24 11:22:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function select_info(sql_post_m){
 }
 
 function next(specialization,special,city,chunk,gorod,city_,spec_){
-	if(special==186){
+	if(special==248){
 		console.log('Парсинг закончен!');
 			return false;
 	}else if(city == gorod.length-1){
